040a5fbd867d6aa0ac85a5360a80572e55e2864c
moxuan-jst <706550717@qq.com>
2020-03-08 17:51:34 +0800

    地图底部footer

---
+++ src/components/MapFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="footer">
+        积极防护，保护自己，戴口罩，勤洗手
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container{
+  background-color: #ffffff;
+  width: 100%;
+  height: 50px;
+}
+
+.footer {
+  text-align: center;
+  line-height: 50px;
+  font-family: 'Microsoft YaHei' serif;
+  font-weight: bold;
+  font-size: 18px;
+}
+</style>
