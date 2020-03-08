cc6fc09552d20441cdca33e1d6526ad7c10990c7
moxuan-jst <706550717@qq.com>
2020-03-08 19:15:52 +0800

    顶部nav

---
+++ src/components/TopNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" id="nav">疫情动态</a>
+      <a class="navbar-brand" href="#" id="nav">迁徙地图</a>
+      <a class="navbar-brand" href="#" id="nav">全民热搜</a>
+      <a class="navbar-brand" href="#" id="nav">实时播报</a>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+#nav {
+  margin-left:200px;
+  margin-right: 150px; 
+  border-bottom: 3px solid #10aeb5
+}
+</style>
