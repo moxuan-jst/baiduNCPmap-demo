682740db42d6f64d2c078a92b349a07aaf855927
moxuan-jst <706550717@qq.com>
2020-03-08 19:16:30 +0800

    添加顶部nav

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,62 @@
 <template>
   <div id="app">
-    <Header/>
+    <Header />
+    <top-nav v-show="show" />
     <div class="container">
       <div class="row">
         <div class="col-md-6 col-md-offset-2">
-          <HelloWorld/>
+          <HelloWorld />
         </div>
       </div>
     </div>
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header/Header.vue'
-import Footer from "./components/footer/Footer.vue"
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header/Header.vue";
+import Footer from "./components/footer/Footer.vue";
+import TopNav from "./components/TopNav.vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      show: false,
+      curHeight: 0
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll, true);
+  },
+  methods: {
+    handleScroll() {
+      var curHeight = Math.floor(
+        document.body.scrollTop ||
+          document.documentElement.scrollTop ||
+          window.pageXOffset
+      );
+      if (curHeight >= 300){
+        this.show = true
+      }else{
+        this.show = false
+      }
+    }
+  },
   components: {
     HelloWorld,
     Header,
-    Footer
+    Footer,
+    TopNav
   }
-}
+};
 </script>
 
 <style>
-#app{
+#app {
   background-color: #10aeb5;
 }
 </style>
