c3b7e9e7378c67bc5db2ae5d05bc61ebcbe795b1
moxuan-jst <706550717@qq.com>
2020-03-08 17:50:26 +0800

    地图底部的tip

---
+++ src/components/footer/Tip.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tip">
+    <p>上百度APP搜索 新型冠状病毒肺炎</p>
+    <p>了解最新进展及权威预防知识</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.tip{
+  margin-top: 40px;
+  height: 100px;
+  text-align: center;
+}
+.tip p{
+  margin: 0;
+  color: azure;
+}
+</style>
