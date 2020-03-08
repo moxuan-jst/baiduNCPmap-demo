8bf0bf3ff87efbb9bcf10fbd78dd1c0e0348280c
moxuan-jst <706550717@qq.com>
2020-03-08 17:49:50 +0800

    添加页脚

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 link">
+        <a href="https://help.baidu.com/" target="_blank">帮助</a>
+        <a href="https://www.baidu.com/search/jubao.html" target="_blank">举报</a>
+        <a href="https://help.baidu.com/newadd?prod_id=1&category=4" target="_blank">用户反馈</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 40px;
+  background-color: #fff;
+}
+.link a{
+  margin-left: 40px;
+  margin-right: 10px;
+  color: #919090;
+  text-decoration: none;
+  text-align: center;
+  line-height: 40px;
+  font-size: 12px;
+}
+.link a:nth-child(1){
+  margin-left: 90px;
+}
+</style>
